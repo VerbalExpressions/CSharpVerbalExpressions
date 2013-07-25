2d690e17b86a5c1c036e81f71ad362d3712dff50
Peder Søholt <psoeholt@yahoo.no>
2013-07-25 20:28:07 +0200

    Initial port with tests and solution

---
+++ VerbalExpressionsUnitTests/VerbalExpressionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using VerbalExpression.Net;
+
+namespace VerbalExpressionsUnitTests
+{
+	[TestClass]
+	public class VerbalExpressionsTests
+	{
+		[TestMethod]
+		public void TestingIfWeHaveAValidURL()
+		{
+			var tester = new VerbalExpressions()
+						.StartOfLine()
+						.Then( "http" )
+						.Maybe( "s" )
+						.Then( "://" )
+						.Maybe( "www." )
+						.AnythingBut( " " )
+						.EndOfLine();
+
+			var testMe = "https://www.google.com";
+
+			// Use RegExp object's native test() function
+			Assert.IsTrue(tester.Test( testMe ), "The URL is incorrect");
+
+			Console.WriteLine("We have a correct URL ");
+		}
+	}
+}
