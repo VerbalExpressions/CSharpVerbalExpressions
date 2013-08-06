4e8b16a75f269c3c16d9a025a1ecf50b51daaa35
@kjellski <otto.kjell@gmail.com>
2013-08-06 23:38:28 +0200

    - removed the reference to the file VerbalExpressionsEnum, which   was gone anyways - added a sealed class for collecting predefined enums that is   a bit more usable then an enum. - refactored the given class and tests to use the new CommonRegex   class.

---
+++ VerbalExpressions/CommonRegex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbalExpression.Net
+{
+
+    /// <summary>
+    /// This class is used to fake an enum. You'll be able to use it as an enum.
+    /// Note: type save enum, found on stackoverflow: http://stackoverflow.com/a/424414/603309
+    /// </summary>
+    public sealed class CommonRegex
+    {
+
+        private readonly String name;
+        private readonly int value;
+
+        public static readonly CommonRegex Url = new CommonRegex(1, @"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[^-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:ww‌​w.|[^-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?‌​(?:[\w]*))?)");
+        public static readonly CommonRegex Email = new CommonRegex(2, @"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}");
+        
+        private CommonRegex(int value, String name)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public int ToSwitchableValue()
+        {
+            return value;
+        }
+
+        public override String ToString()
+        {
+            return name;
+        }
+
+    }
+}
--- VerbalExpressionsUnitTests/VerbalExpressionsTests.cs
+++ VerbalExpressionsUnitTests/VerbalExpressionsTests.cs
@@ -5,211 +5,211 @@ using VerbalExpression.Net;
 
 namespace VerbalExpressionsUnitTests
 {
-	[TestClass]
-	public class VerbalExpressionsTests
-	{
+    [TestClass]
+    public class VerbalExpressionsTests
+    {
 
-		private VerbalExpressions verbEx;
+        private VerbalExpressions verbEx;
 
-		[TestInitialize]
-		public void Initialize()
-		{
-			verbEx = new VerbalExpressions();
-		}
+        [TestInitialize]
+        public void Initialize()
+        {
+            verbEx = new VerbalExpressions();
+        }
 
-		[TestMethod]
-		public void TestingIfWeHaveAValidURL()
-		{
-			verbEx = new VerbalExpressions()
-						.StartOfLine()
-						.Then( "http" )
-						.Maybe( "s" )
-						.Then( "://" )
-						.Maybe( "www." )
-						.AnythingBut( " " )
+        [TestMethod]
+        public void TestingIfWeHaveAValidURL()
+        {
+            verbEx = new VerbalExpressions()
+                        .StartOfLine()
+                        .Then( "http" )
+                        .Maybe( "s" )
+                        .Then( "://" )
+                        .Maybe( "www." )
+                        .AnythingBut( " " )
                         .Then(".com")
-						.EndOfLine();
+                        .EndOfLine();
 
             var testMe = "https://www.google.com";
-			Assert.IsTrue(verbEx.Test( testMe ), "The URL is incorrect");
-			Console.WriteLine("We have a correct URL ");
-		}
-	
-		[TestMethod]
-		public void StartOfLine_CreatesCorrectRegex()
-		{
-			verbEx.StartOfLine();
-			Assert.AreEqual("^", verbEx.ToString(), "missing start of line regex");
-		}
-	
-		[TestMethod]
-		public void StartOfLine_ThenHttpMaybeWww_DoesMatchHttpInStart()
-		{
-			verbEx.StartOfLine()
-				.Then("http")
-				.Maybe("www");
-
-			var isMatch = Regex.IsMatch("http", verbEx.ToString());
-			Assert.IsTrue(isMatch, "Should match http in start");
-		}
-	
-		[TestMethod]
-		public void StartOfLine_ThenHttpMaybeWww_DoesNotMatchWwwInStart()
-		{
-			verbEx.StartOfLine()
-				.Then("http")
-				.Maybe("www");
-
-			var isMatch = verbEx.IsMatch("www");
-			Assert.IsFalse(isMatch, "Should not match www in start");
-		}
-	
-		[TestMethod]
-		public void EndOfLine_AddDotComtEndOfLine_DoesMatchDotComInEnd()
-		{
-			verbEx.Add(".com")
-				.EndOfLine();
-
-			var isMatch = verbEx.IsMatch("www.google.com");
-			Assert.IsTrue(isMatch, "Should match '.com' in end");
-		}
-	
-		[TestMethod]
-		public void EndOfLine_AddDotComEndOfLine_DoesNotMatchSlashInEnd()
-		{
-			verbEx.Add(".com")
-				.EndOfLine();
-
-			var isMatch = verbEx.IsMatch("http://www.google.com/");
-			Assert.IsFalse(isMatch, "Should not match '/' in end");
-		}
-
-		[TestMethod]
-		public void Add_AddDotCom_DoesNotMatchGoogleComWithoutDot()
-		{
-			verbEx.Add(".com");
-
-			var isMatch = verbEx.IsMatch("http://www.googlecom/");
-			Assert.IsFalse(isMatch, "Should not match 'ecom'");
-		}
-		
+            Assert.IsTrue(verbEx.Test( testMe ), "The URL is incorrect");
+            Console.WriteLine("We have a correct URL ");
+        }
+    
         [TestMethod]
-		public void Or_AddComOrOrg_DoesMatchComAndOrg()
-		{
-			verbEx.Add("com").Or("org");
+        public void StartOfLine_CreatesCorrectRegex()
+        {
+            verbEx.StartOfLine();
+            Assert.AreEqual("^", verbEx.ToString(), "missing start of line regex");
+        }
+    
+        [TestMethod]
+        public void StartOfLine_ThenHttpMaybeWww_DoesMatchHttpInStart()
+        {
+            verbEx.StartOfLine()
+                .Then("http")
+                .Maybe("www");
+
+            var isMatch = Regex.IsMatch("http", verbEx.ToString());
+            Assert.IsTrue(isMatch, "Should match http in start");
+        }
+    
+        [TestMethod]
+        public void StartOfLine_ThenHttpMaybeWww_DoesNotMatchWwwInStart()
+        {
+            verbEx.StartOfLine()
+                .Then("http")
+                .Maybe("www");
+
+            var isMatch = verbEx.IsMatch("www");
+            Assert.IsFalse(isMatch, "Should not match www in start");
+        }
+    
+        [TestMethod]
+        public void EndOfLine_AddDotComtEndOfLine_DoesMatchDotComInEnd()
+        {
+            verbEx.Add(".com")
+                .EndOfLine();
+
+            var isMatch = verbEx.IsMatch("www.google.com");
+            Assert.IsTrue(isMatch, "Should match '.com' in end");
+        }
+    
+        [TestMethod]
+        public void EndOfLine_AddDotComEndOfLine_DoesNotMatchSlashInEnd()
+        {
+            verbEx.Add(".com")
+                .EndOfLine();
+
+            var isMatch = verbEx.IsMatch("http://www.google.com/");
+            Assert.IsFalse(isMatch, "Should not match '/' in end");
+        }
+
+        [TestMethod]
+        public void Add_AddDotCom_DoesNotMatchGoogleComWithoutDot()
+        {
+            verbEx.Add(".com");
+
+            var isMatch = verbEx.IsMatch("http://www.googlecom/");
+            Assert.IsFalse(isMatch, "Should not match 'ecom'");
+        }
+        
+        [TestMethod]
+        public void Or_AddComOrOrg_DoesMatchComAndOrg()
+        {
+            verbEx.Add("com").Or("org");
 
             Console.WriteLine(verbEx);
             Assert.IsTrue(verbEx.IsMatch("org"), "Should match 'org'");
             Assert.IsTrue(verbEx.IsMatch("com"), "Should match 'com'");
-		}
-	    
+        }
+        
         [TestMethod]
-		public void Or_AddComOrOrg_RegexIsAsExpecteds()
-		{
-			verbEx.Add("com").Or("org");
+        public void Or_AddComOrOrg_RegexIsAsExpecteds()
+        {
+            verbEx.Add("com").Or("org");
             
             Assert.AreEqual("(com)|(org)", verbEx.ToString());
-		}
-	
-		[TestMethod]
-		public void Anything_StartOfLineAnythingEndOfline_DoesMatchAnyThing()
-		{
-			verbEx
-				.StartOfLine()
-				.Anything()
-				.EndOfLine();
-			
-			var isMatch = verbEx.IsMatch("'!@#$%¨&*()__+{}'");
-			Assert.IsTrue(isMatch, "Ooops, should match anything");
-		}
-	
-		[TestMethod]
-		public void WithAnyCase_AddwwwWithAnyCase_DoesMatchwWw()
-		{
-			verbEx.Add("www")
-				.WithAnyCase();
-			
-			var isMatch = verbEx.IsMatch("wWw");
-			Assert.IsTrue(isMatch, "Should match any case");
-		}
-	
-		[TestMethod]
-		public void WithAnyCase_SetsCorrectIgnoreCaseRegexOptionAndHasMultiLineRegexOptionAsDefault()
-		{
-			verbEx.WithAnyCase();
-			
-			var regex = verbEx.ToRegex();
-			Assert.IsTrue(regex.Options.HasFlag(RegexOptions.IgnoreCase), "RegexOptions should have ignoreCase");
-			Assert.IsTrue(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should have MultiLine as default");
-		}
-	
-		[TestMethod]
-		public void RemoveModifier_RemoveModifierM_RemovesMulitilineAsDefault()
-		{
-			var regex = verbEx.ToRegex();
-			Assert.IsTrue(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should have MultiLine as default");
-
-			verbEx.RemoveModifier('m');
-			regex = verbEx.ToRegex();
-
-			Assert.IsFalse(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should now have been removed");
-		}
-	
-		[TestMethod]
-		public void WithAnyCase_AddwwwWithAnyCaseFalse_DoesNotMatchwWw()
-		{
-			verbEx.Add("www")
-				.WithAnyCase(false);
-			
-			var isMatch = verbEx.IsMatch("wWw");
-			Assert.IsFalse(isMatch, "Should not match any case");
-		}
-	
-		[TestMethod]
-		public void Then_VerbalExpressionsEmail_DoesMatchEmail()
-		{
-			verbEx.StartOfLine().Then(VerbalExpressions.email);
-			
-			var isMatch = verbEx.IsMatch("test@github.com");
-			Assert.IsTrue(isMatch, "Should match email address");
-		}
-		
+        }
+    
         [TestMethod]
-		public void Then_VerbalExpressionsEmail_DoesNotMatchUrl()
-		{
-			verbEx.StartOfLine().Then(VerbalExpressions.email);
-			
-			var isMatch = verbEx.IsMatch("http://www.google.com");
-			Assert.IsFalse(isMatch, "Should not match url address");
-		}
-	
-		[TestMethod]
-		public void Then_VerbalExpressionsUrl_DoesMatchUrl()
-		{
-			verbEx.StartOfLine().Then(VerbalExpressions.url);
+        public void Anything_StartOfLineAnythingEndOfline_DoesMatchAnyThing()
+        {
+            verbEx
+                .StartOfLine()
+                .Anything()
+                .EndOfLine();
+            
+            var isMatch = verbEx.IsMatch("'!@#$%¨&*()__+{}'");
+            Assert.IsTrue(isMatch, "Ooops, should match anything");
+        }
+    
+        [TestMethod]
+        public void WithAnyCase_AddwwwWithAnyCase_DoesMatchwWw()
+        {
+            verbEx.Add("www")
+                .WithAnyCase();
+            
+            var isMatch = verbEx.IsMatch("wWw");
+            Assert.IsTrue(isMatch, "Should match any case");
+        }
+    
+        [TestMethod]
+        public void WithAnyCase_SetsCorrectIgnoreCaseRegexOptionAndHasMultiLineRegexOptionAsDefault()
+        {
+            verbEx.WithAnyCase();
+            
+            var regex = verbEx.ToRegex();
+            Assert.IsTrue(regex.Options.HasFlag(RegexOptions.IgnoreCase), "RegexOptions should have ignoreCase");
+            Assert.IsTrue(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should have MultiLine as default");
+        }
+    
+        [TestMethod]
+        public void RemoveModifier_RemoveModifierM_RemovesMulitilineAsDefault()
+        {
+            var regex = verbEx.ToRegex();
+            Assert.IsTrue(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should have MultiLine as default");
 
-		    Assert.IsTrue(verbEx.IsMatch("http://www.google.com"), "Should match url address");
-		    Assert.IsTrue(verbEx.IsMatch("https://www.google.com"), "Should match url address");
-		    Assert.IsTrue(verbEx.IsMatch("http://google.com"), "Should match url address");
-		}	
+            verbEx.RemoveModifier('m');
+            regex = verbEx.ToRegex();
 
-		[TestMethod]
-		public void Then_VerbalExpressionsUrl_DoesNotMatchEmail()
-		{
-			verbEx.StartOfLine().Then(VerbalExpressions.url);
+            Assert.IsFalse(regex.Options.HasFlag(RegexOptions.Multiline), "RegexOptions should now have been removed");
+        }
+    
+        [TestMethod]
+        public void WithAnyCase_AddwwwWithAnyCaseFalse_DoesNotMatchwWw()
+        {
+            verbEx.Add("www")
+                .WithAnyCase(false);
+            
+            var isMatch = verbEx.IsMatch("wWw");
+            Assert.IsFalse(isMatch, "Should not match any case");
+        }
+    
+        [TestMethod]
+        public void Then_VerbalExpressionsEmail_DoesMatchEmail()
+        {
+            verbEx.StartOfLine().Then(CommonRegex.Email);
+            
+            var isMatch = verbEx.IsMatch("test@github.com");
+            Assert.IsTrue(isMatch, "Should match email address");
+        }
+        
+        [TestMethod]
+        public void Then_VerbalExpressionsEmail_DoesNotMatchUrl()
+        {
+            verbEx.StartOfLine().Then(CommonRegex.Email);
+            
+            var isMatch = verbEx.IsMatch("http://www.google.com");
+            Assert.IsFalse(isMatch, "Should not match url address");
+        }
+    
+        [TestMethod]
+        public void Then_VerbalExpressionsUrl_DoesMatchUrl()
+        {
+            verbEx.StartOfLine().Then(CommonRegex.Url);
 
-		    Assert.IsFalse(verbEx.IsMatch("test@github.com"), "Should not match email address");
-		}
+            Assert.IsTrue(verbEx.IsMatch("http://www.google.com"), "Should match url address");
+            Assert.IsTrue(verbEx.IsMatch("https://www.google.com"), "Should match url address");
+            Assert.IsTrue(verbEx.IsMatch("http://google.com"), "Should match url address");
+        }	
 
-		[TestMethod]
-		public void Or_VerbalExpressionsUrlOrVerbalExpressionEmail_DoesMatchEmailAndUrl()
-		{
-			verbEx.Add(VerbalExpressions.url)
-                .Or(VerbalExpressionsEnum.email);
+        [TestMethod]
+        public void Then_VerbalExpressionsUrl_DoesNotMatchEmail()
+        {
+            verbEx.StartOfLine().Then(CommonRegex.Url);
+
+            Assert.IsFalse(verbEx.IsMatch("test@github.com"), "Should not match email address");
+        }
+
+        [TestMethod]
+        public void Or_VerbalExpressionsUrlOrVerbalExpressionEmail_DoesMatchEmailAndUrl()
+        {
+            verbEx.Add(CommonRegex.Url)
+                .Or(CommonRegex.Email);
 
             Console.WriteLine(verbEx);
-		    Assert.IsTrue(verbEx.IsMatch("test@github.com"), "Should match email address");
-		    Assert.IsTrue(verbEx.IsMatch("http://www.google.com"), "Should match url address");
-		}
-	}
+            Assert.IsTrue(verbEx.IsMatch("test@github.com"), "Should match email address");
+            Assert.IsTrue(verbEx.IsMatch("http://www.google.com"), "Should match url address");
+        }
+    }
 }
