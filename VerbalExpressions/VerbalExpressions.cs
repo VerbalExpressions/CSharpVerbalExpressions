8fbec4a4ec00992584780d857a360c436b58f8e4
Peder Søholt <psoeholt@yahoo.no>
2013-08-10 02:43:10 -1000

    Merge from AlexPeta. Fixed default parameters and spacing between methods

--- VerbalExpressions/VerbalExpressions.cs
+++ VerbalExpressions/VerbalExpressions.cs
@@ -11,6 +11,7 @@
  * 
  * Date: 2013-08-06
  */
+
 using System;
 using System.Text;
 using System.Linq;
@@ -21,55 +22,57 @@ namespace CSharpVerbalExpressions
     public class VerbalExpressions
     {
         #region Statics
+
         /// <summary>
         /// Returns a default instance of VerbalExpressions
         /// having the Multiline option enabled
         /// </summary>
         public static VerbalExpressions DefaultExpression
         {
-            get
-            {
-                return new VerbalExpressions();
-            }
+            get { return new VerbalExpressions(); }
         }
+
         #endregion Statics
 
         #region Private Members
+
         private string _prefixes = "";
         private string _source = "";
         private string _suffixes = "";
         private RegexOptions _modifiers = RegexOptions.Multiline;
+
         #endregion Private Members
 
         #region Private Properties
+
         private string RegexString
         {
-            get
-            {
-                return _prefixes + _source + _suffixes;
-            }
+            get { return _prefixes + _source + _suffixes; }
         }
+
         private Regex PatternRegex
         {
-            get
-            {
-                return new Regex(this.RegexString, _modifiers);
-            }
+            get { return new Regex(this.RegexString, _modifiers); }
         }
+
         #endregion Private Properties
 
         #region Constructors
+
         private VerbalExpressions()
         {
         }
+
         static VerbalExpressions()
         {
         }
+
         #endregion Constructors
 
         #region Public Methods
 
         #region Helpers
+
         public string Sanitize(string value)
         {
             if (string.IsNullOrEmpty(value))
@@ -84,21 +87,26 @@ namespace CSharpVerbalExpressions
         {
             return IsMatch(toTest);
         }
+
         public bool IsMatch(string toTest)
         {
             return PatternRegex.IsMatch(toTest);
         }
+
         public Regex ToRegex()
         {
             return PatternRegex;
         }
+
         public override string ToString()
         {
             return PatternRegex.ToString();
         }
+
         #endregion Helpers
 
         #region Expression Modifiers
+
         public VerbalExpressions Add(string value)
         {
             if (object.ReferenceEquals(value, null))
@@ -108,10 +116,12 @@ namespace CSharpVerbalExpressions
 
             return Add(value, true);
         }
+
         public VerbalExpressions Add(CommonRegex commonRegex)
         {
             return Add(commonRegex.Name, false);
         }
+
         public VerbalExpressions Add(string value, bool sanitize = true)
         {
             if (value == null)
@@ -121,64 +131,60 @@ namespace CSharpVerbalExpressions
             _source += value;
             return this;
         }
+
         public VerbalExpressions StartOfLine(bool enable = true)
         {
             _prefixes = enable ? "^" : string.Empty;
             return this;
         }
+
         public VerbalExpressions EndOfLine(bool enable = true)
         {
             _suffixes = enable ? "$" : string.Empty;
             return this;
         }
-        public VerbalExpressions Then(string value)
-        {
-            value = string.Format("({0})", Sanitize(value));
-            return Then(value,false);
-        }
-        public VerbalExpressions Then(string value, bool sanitize)
+
+        public VerbalExpressions Then(string value, bool sanitize = true)
         {
             var sanitizedValue = sanitize ? Sanitize(value) : value;
             value = string.Format("({0})", sanitizedValue);
             return Add(value, false);
         }
+
         public VerbalExpressions Then(CommonRegex commonRegex)
         {
             return Then(commonRegex.Name, false);
         }
+
         public VerbalExpressions Find(string value)
         {
             return Then(value);
         }
-        public VerbalExpressions Maybe(string value)
-        {
-            value = string.Format("({0})?",Sanitize(value));
-            return Add(value,false);
-        }
-        public VerbalExpressions Maybe(string value, bool sanitize)
+
+        public VerbalExpressions Maybe(string value, bool sanitize = true)
         {
             value = sanitize ? Sanitize(value) : value;
-            value = string.Format("({0})?", Sanitize(value));
+            value = string.Format("({0})?", value);
             return Add(value, false);
         }
+
         public VerbalExpressions Maybe(CommonRegex commonRegex)
         {
             return Maybe(commonRegex.Name, sanitize: false);
         }
+
         public VerbalExpressions Anything()
         {
-            return Add("(.*)",false);
+            return Add("(.*)", false);
         }
-        public VerbalExpressions AnythingBut(string value)
-        {
-            return AnythingBut(value, true);
-        }
-        public VerbalExpressions AnythingBut(string value, bool sanitize)
+
+        public VerbalExpressions AnythingBut(string value, bool sanitize = true)
         {
             value = sanitize ? Sanitize(value) : value;
             value = string.Format("([^{0}]*)", value);
-            return Add(value,false);
+            return Add(value, false);
         }
+
         public VerbalExpressions Replace(string value)
         {
             string whereToReplace = PatternRegex.ToString();
@@ -190,23 +196,28 @@ namespace CSharpVerbalExpressions
 
             return this;
         }
+
         public VerbalExpressions LineBreak()
         {
             return Add(@"(\n|(\r\n))", false);
         }
+
         public VerbalExpressions Br()
         {
             return LineBreak();
         }
+
         public VerbalExpressions Tab()
         {
             return Add(@"\t");
         }
+
         public VerbalExpressions Word()
         {
             return Add(@"\w+", false);
         }
-        public VerbalExpressions AnyOf(string value,bool sanitize)
+
+        public VerbalExpressions AnyOf(string value, bool sanitize = true)
         {
             if (string.IsNullOrEmpty(value))
             {
@@ -217,14 +228,12 @@ namespace CSharpVerbalExpressions
             value = string.Format("[{0}]", Sanitize(value));
             return Add(value, false);
         }
-        public VerbalExpressions AnyOf(string value)
-        {
-            return this.AnyOf(value, true);
-        }
+
         public VerbalExpressions Any(string value)
         {
             return AnyOf(value);
         }
+
         public VerbalExpressions Range(params object[] arguments)
         {
             if (object.ReferenceEquals(arguments, null))
@@ -236,7 +245,7 @@ namespace CSharpVerbalExpressions
             {
                 throw new ArgumentOutOfRangeException("arguments");
             }
-            
+
             string[] sanitizedStrings = arguments.Select(argument =>
                 {
                     if (object.ReferenceEquals(argument, null))
@@ -254,17 +263,17 @@ namespace CSharpVerbalExpressions
                         return Sanitize(casted);
                     }
                 })
-                .Where(sanitizedString => !string.IsNullOrEmpty(sanitizedString))
-                .OrderBy(s => s)
-                .ToArray();
+                                                 .Where(sanitizedString => !string.IsNullOrEmpty(sanitizedString))
+                                                 .OrderBy(s => s)
+                                                 .ToArray();
 
             if (!sanitizedStrings.Any())
             {
                 return this;
             }
 
-            bool hasOddNumberOfParams = (sanitizedStrings.Length % 2) > 0;
-            
+            bool hasOddNumberOfParams = (sanitizedStrings.Length%2) > 0;
+
             StringBuilder sb = new StringBuilder("[");
             for (int _from = 0; _from < sanitizedStrings.Length; _from += 2)
             {
@@ -282,13 +291,10 @@ namespace CSharpVerbalExpressions
                 sb.AppendFormat("|{0}", sanitizedStrings.Last());
             }
 
-            return Add(sb.ToString(),false);
+            return Add(sb.ToString(), false);
         }
-        public VerbalExpressions Multiple(string value)
-        {
-            return Multiple(value, true);
-        }
-        public VerbalExpressions Multiple(string value, bool sanitize)
+
+        public VerbalExpressions Multiple(string value, bool sanitize = true)
         {
             if (string.IsNullOrEmpty(value))
             {
@@ -300,15 +306,13 @@ namespace CSharpVerbalExpressions
 
             return Add(value, false);
         }
-        public VerbalExpressions Or(string value)
-        {
-            return Or(value, true);
-        }
+
         public VerbalExpressions Or(CommonRegex commonRegex)
         {
-            return Or(commonRegex.Name,false);
+            return Or(commonRegex.Name, false);
         }
-        public VerbalExpressions Or(string value, bool sanitize)
+
+        public VerbalExpressions Or(string value, bool sanitize = true)
         {
             if (_prefixes.IndexOf("(") == -1)
                 _prefixes += "(";
@@ -319,16 +323,18 @@ namespace CSharpVerbalExpressions
 
             return Add(value, sanitize);
         }
+
         #endregion Expression Modifiers
 
         #region Expression Options Modifiers
+
         public VerbalExpressions AddModifier(char modifier)
         {
             switch (modifier)
             {
-                //case 'd':
-                //	_modifiers |= RegexOptions.UNIX_LINES;
-                //	break;
+                    //case 'd':
+                    //	_modifiers |= RegexOptions.UNIX_LINES;
+                    //	break;
                 case 'i':
                     _modifiers |= RegexOptions.IgnoreCase;
                     break;
@@ -338,26 +344,27 @@ namespace CSharpVerbalExpressions
                 case 'm':
                     _modifiers |= RegexOptions.Multiline;
                     break;
-                //case 's':
-                //	_modifiers |= RegexOptions.DOTALL;
-                //	break;
-                //case 'u':
-                //	_modifiers |= Pattern.UNICODE_CASE;
-                //	break;
-                //case 'U':
-                //	_modifiers |= Pattern.UNICODE_CHARACTER_CLASS;
-                //	break;
+                    //case 's':
+                    //	_modifiers |= RegexOptions.DOTALL;
+                    //	break;
+                    //case 'u':
+                    //	_modifiers |= Pattern.UNICODE_CASE;
+                    //	break;
+                    //case 'U':
+                    //	_modifiers |= Pattern.UNICODE_CHARACTER_CLASS;
+                    //	break;
             }
 
             return this;
         }
+
         public VerbalExpressions RemoveModifier(char modifier)
         {
             switch (modifier)
             {
-                //case 'd':
-                //	_modifiers &= ~Pattern.UNIX_LINES;
-                //	break;
+                    //case 'd':
+                    //	_modifiers &= ~Pattern.UNIX_LINES;
+                    //	break;
                 case 'i':
                     _modifiers &= ~RegexOptions.IgnoreCase;
                     break;
@@ -367,21 +374,22 @@ namespace CSharpVerbalExpressions
                 case 'm':
                     _modifiers &= ~RegexOptions.Multiline;
                     break;
-                //case 's':
-                //	_modifiers &= ~Pattern.DOTALL;
-                //	break;
-                //case 'u':
-                //	_modifiers &= ~Pattern.UNICODE_CASE;
-                //	break;
-                //case 'U':
-                //	_modifiers &= ~Pattern.UNICODE_CHARACTER_CLASS;
-                //	break;
-                //default:
-                //	break;
+                    //case 's':
+                    //	_modifiers &= ~Pattern.DOTALL;
+                    //	break;
+                    //case 'u':
+                    //	_modifiers &= ~Pattern.UNICODE_CASE;
+                    //	break;
+                    //case 'U':
+                    //	_modifiers &= ~Pattern.UNICODE_CHARACTER_CLASS;
+                    //	break;
+                    //default:
+                    //	break;
             }
 
             return this;
         }
+
         public VerbalExpressions WithAnyCase(bool enable = true)
         {
             if (enable)
@@ -394,6 +402,7 @@ namespace CSharpVerbalExpressions
             }
             return this;
         }
+
         public VerbalExpressions UseOneLineSearchOption(bool enable)
         {
             if (enable)
@@ -407,12 +416,15 @@ namespace CSharpVerbalExpressions
 
             return this;
         }
+
         public VerbalExpressions WithOptions(RegexOptions options)
         {
             this._modifiers = options;
             return this;
         }
+
         #endregion Expression Options Modifiers
+
         #endregion Public Methods
     }
 }
