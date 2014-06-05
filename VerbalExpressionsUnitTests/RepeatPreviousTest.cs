6a634d9b884557bd53009af9cf7b6f80a3eee111
Alex <AlejandroDguez@outlook.es>
2014-06-05 17:36:17 +0200

    · Add RepeatPrevious method.

---
+++ VerbalExpressionsUnitTests/RepeatPreviousTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using CSharpVerbalExpressions;
+using NUnit.Framework;
+
+namespace VerbalExpressionsUnitTests
+{
+    [TestFixture]
+    public class RepeatPreviousTests
+    {
+
+        [Test]
+        public void RepeatPrevious_WhenThreeA_RegesIsAsExpected()
+        {
+            // Arrange
+            VerbalExpressions verbEx = VerbalExpressions.DefaultExpression;
+
+            // Act
+            verbEx.BeginCapture()
+                  .Add("A")
+                  .RepeatPrevious(3)
+                  .EndCapture();
+
+            // Assert
+            Assert.AreEqual("(A{3})", verbEx.ToString());
+        }
+
+        [Test]
+        public void RepeatPrevious_WhenBetweenTwoAndFourA_RegesIsAsExpected()
+        {
+            // Arrange
+            VerbalExpressions verbEx = VerbalExpressions.DefaultExpression;
+
+            // Act
+            verbEx.BeginCapture()
+                  .Add("A")
+                  .RepeatPrevious(2,4)
+                  .EndCapture();
+
+            // Assert
+            Assert.AreEqual("(A{2,4})", verbEx.ToString());
+        }
+
+    }
+}
