f1987790923bdff66c63b256b2e2c8daa84b6e40
Alex <AlejandroDguez@outlook.es>
2014-06-06 12:16:09 +0200

    · Add BeginCapture with a name of group. · Add a Capture method to retrieve value of a name group.

--- VerbalExpressionsUnitTests/CaptureTests.cs
+++ VerbalExpressionsUnitTests/CaptureTests.cs
@@ -39,6 +39,25 @@ namespace VerbalExpressionsUnitTests {
             Assert.AreEqual( @"(\w+)\s(\1)", verbEx.ToString() );
             Assert.IsTrue( verbEx.Test( TEST_STRING ), "There is no duplicates in the textString." );
         }
+
+        [Test]
+        public void BeginCaptureWithName_CreateRegexGroupNameAsExpected()
+        {
+            // Arrange
+            VerbalExpressions verbEx = VerbalExpressions.DefaultExpression;
+
+            // Act
+            verbEx.Add("COD")
+                .BeginCapture("GroupNumber")
+                .Any("0-9")
+                .RepeatPrevious(3)
+                .EndCapture()
+                .Add("END");
+
+            // Assert
+            Assert.AreEqual( @"COD(?<GroupNumber>[0-9]{3})END",verbEx.ToString());
+            Assert.AreEqual( "123", verbEx.Capture("COD123END","GroupNumber"));
+        }
     }
 
 }
