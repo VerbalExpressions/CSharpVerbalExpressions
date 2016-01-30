1ae416f9de098d39a0de8ae89bc374f6b98fcf12
SignpostMarv <me@signpostmarv.name>
2016-01-30 18:11:38 +0000

    running format document on all files

--- VerbalExpressionsUnitTests/EndOfLineTests.cs
+++ VerbalExpressionsUnitTests/EndOfLineTests.cs
@@ -3,27 +3,27 @@ using NUnit.Framework;
 
 namespace VerbalExpressionsUnitTests
 {
-	[TestFixture]
-	public class EndOfLineTests
-	{
-		[Test]
-		public void EndOfLine_AddDotComtEndOfLine_DoesMatchDotComInEnd()
-		{
-			var verbEx = VerbalExpressions.DefaultExpression;
-			verbEx.Add(".com").EndOfLine();
+    [TestFixture]
+    public class EndOfLineTests
+    {
+        [Test]
+        public void EndOfLine_AddDotComtEndOfLine_DoesMatchDotComInEnd()
+        {
+            var verbEx = VerbalExpressions.DefaultExpression;
+            verbEx.Add(".com").EndOfLine();
 
-			var isMatch = verbEx.IsMatch("www.google.com");
-			Assert.IsTrue(isMatch, "Should match '.com' in end");
-		}
+            var isMatch = verbEx.IsMatch("www.google.com");
+            Assert.IsTrue(isMatch, "Should match '.com' in end");
+        }
 
-		[Test]
-		public void EndOfLine_AddDotComEndOfLine_DoesNotMatchSlashInEnd()
-		{
-			var verbEx = VerbalExpressions.DefaultExpression;
-			verbEx.Add(".com").EndOfLine();
+        [Test]
+        public void EndOfLine_AddDotComEndOfLine_DoesNotMatchSlashInEnd()
+        {
+            var verbEx = VerbalExpressions.DefaultExpression;
+            verbEx.Add(".com").EndOfLine();
 
-			var isMatch = verbEx.IsMatch("http://www.google.com/");
-			Assert.IsFalse(isMatch, "Should not match '/' in end");
-		}
-	}
+            var isMatch = verbEx.IsMatch("http://www.google.com/");
+            Assert.IsFalse(isMatch, "Should not match '/' in end");
+        }
+    }
 }
